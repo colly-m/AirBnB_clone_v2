b44c3dd17e38af1bcf57735d852d8380315f7ae5
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-01-17 01:02:05 -0800

    filing

--- console.py
+++ console.py
@@ -40,7 +40,7 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
-    def do_create(self, line):
+    def do_create(self, arg):
         """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
         Create a new class instance with given keys/values and print its id.
         """
