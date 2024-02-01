62ff796fc7a93528b59baf0919ec29053e6812e3
colly-m <sindanicolly@gmail.com>
2024-02-01 13:11:42 -0800

    Fab compressor

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Fabric script to genereate tgz archive from web_static content
+"""
+import os
+from datetime import datetime
+from fabric.api import *
+
+
+def do_pack():
+    """Module to make an archive on web_static folder"""
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
+        return None
