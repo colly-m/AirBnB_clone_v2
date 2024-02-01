27e132b332e66efe3641961fd2ee906bced97908
colly-m <sindanicolly@gmail.com>
2024-02-01 12:48:01 -0800

    Server's prep

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,11 +7,8 @@ sudo apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
 
 # Creates necessary directories if not present
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 
 # Creates a fake html file
 sudo touch /data/web_static/releases/test/index.html
@@ -20,7 +17,7 @@ sudo echo "<html><head></head><body>Colly-m page</body></html>" | sudo tee /data
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Gives ownership to ubuntu user ond group
-sudo chown -R ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Updates Nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
