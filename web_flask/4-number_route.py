3fc7be8cef1d7ffd8563d987a96b160ac68b8dec
colly-m <sindanicolly@gmail.com>
2024-02-18 05:12:25 -0800

    Num check

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Module to start a Flask web application
+That listens on 0.0.0.0, port 5000.
+Routes: Then Displays 'Hello HBNB!', 'HBNB', 'C', and 'Python'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Define the route for the root
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """Displays C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """Displays Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def isNum(n):
+    """Displays “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
